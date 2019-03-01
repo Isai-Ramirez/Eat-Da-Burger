fc74ec1f081de871e5b547f6a65061af3bdc2c3c
isai <Isai4827@4827@gmail.com>
2019-03-01 11:57:55 -0700

    ...

--- config/connection.js
+++ config/connection.js
@@ -1,11 +1,11 @@
 var mysql = require("mysql");
 var connection; 
 
-if( process.env.JAWSDB_URL){
+if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
   var connection = mysql.createConnection({
-  host: "localhost",
+  host: "l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
   port: 3306,
   user: "hcgyf9c76ma3tiv5",
   password: "st3hs8tewv7odphd",
--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,9 +17,9 @@ router.get("/", function(req, res) {
 
 router.post("/api/burgers", function(req, res) {
   burger.create([
-    "name", "sleepy"
+    "name", "devoured"
   ], [
-    req.body.name, req.body.sleepy
+    req.body.name, req.body.devoured
   ], function(result) {
     res.json({ id: result.insertId });
   });
@@ -31,7 +31,7 @@ router.put("/api/burgers/:id", function(req, res) {
   console.log("condition", condition);
 
   burger.update({
-    sleepy: req.body.sleepy
+    devoured: req.body.devoured
   }, condition, function(result) {
     if (result.changedRows == 0) {
       return res.status(404).end();
