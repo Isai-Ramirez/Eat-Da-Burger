dd81ebeef922f1470cda75a1691d774c99f9d266
isai <Isai4827@4827@gmail.com>
2019-03-01 13:04:38 -0700

    Deploying.

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,11 @@ if(process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 }else{
   var connection = mysql.createConnection({
-  host: "l7cup2om0gngra77.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  host: "localhost",
   port: 3306,
-  user: "hcgyf9c76ma3tiv5",
-  password: "st3hs8tewv7odphd",
-  database: "k34nslmog7x6ghvg"
+  user: "root",
+  password: "root",
+  database: "burgers"
 });
 };
 
